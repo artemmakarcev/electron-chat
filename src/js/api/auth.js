73bb7a812cfdfaf37e04c7be18ed3745f4cc5146
artemmakarcev <makarcev4@gmail.com>
2022-08-11 16:44:40 +0500

    fix

--- src/js/api/auth.js
+++ src/js/api/auth.js
@@ -1,24 +1,20 @@
-
-
-
 import db from '../db/firestore';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const createUserProfile = userProfile =>
-  db
-    .collection('profiles')
-    .doc(userProfile.uid)
-    .set(userProfile)
+const createUserProfile = (userProfile) => db.collection('profiles').doc(userProfile.uid).set(userProfile);
 
-export async function register({email, password, username, avatar}) {
+export async function register({ email, password, username, avatar }) {
   try {
     const { user } = await firebase.auth().createUserWithEmailAndPassword(email, password);
-    await createUserProfile({uid: user.uid, username, email, avatar, joinedChats: []})
-  } catch(error) {
+    await createUserProfile({ uid: user.uid, username, email, avatar, joinedChats: [] });
+  } catch (error) {
     return Promise.reject(error.message);
   }
 }
 
-export const onAuthStateChanges = onAuth =>
-  firebase.auth().onAuthStateChanged(onAuth)
+export const login = ({ email, password }) => firebase.auth().signInWithEmailAndPassword(email, password);
+
+export const logout = () => firebase.auth().signOut();
+
+export const onAuthStateChanges = (onAuth) => firebase.auth().onAuthStateChanged(onAuth);
--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { logout } from '../api/auth';
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector(({ auth }) => auth.user);
   return (
     <div className="chat-navbar">
       <nav className="chat-navbar-inner">
@@ -16,9 +20,17 @@ export default function Navbar() {
         </div>
         <div className="chat-navbar-inner-right">
           <span className="logged-in-user">Hi User</span>
-          <button onClick={() => navigate('/login')} className="btn btn-outline-success ml-2">
+{/*           <button onClick={() => navigate('/')} className="btn btn-outline-success ml-2">
             Login
-          </button>
+          </button> */}
+          <Link
+            to="/"
+            className="btn btn-outline-success ml-2">Login</Link>
+          {user && (
+            <button onClick={() => dispatch(logout())} className="btn btn-outline-danger ml-2">
+              Logout
+            </button>
+          )}
         </div>
       </nav>
     </div>
